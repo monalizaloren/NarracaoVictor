3d13f5d4d02a45cae128f8124a8c9d673dbc90d3
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-12-01 15:55:43 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const StackNav = () => {
+  return(
+  <Stack.Navigator initialRouteName="Login"  screenOptions={{
+    headerShown: false,
+    gestureEnabled: false
+  }}>
+    <Stack.Screen name="Login" component={LoginScreen} />
+    
+  </Stack.Navigator>)
+}
